edf38242df01a19c6d44c55d83e7af0548636f97
Prasanna Anjankar <prasannaanjankar@gmail.com>
2022-10-10 21:21:32 +0530

    abc

--- apnikaksha+Anuj bhaiyya DSA+striver 180/Trees and Graph/ans.cpp
+++ apnikaksha+Anuj bhaiyya DSA+striver 180/Trees and Graph/ans.cpp
@@ -14,40 +14,29 @@ class Node{
 };
 
 
-vector<int> inorderTraversal(Node* root){
-    vector<int> v;
-    if(root==NULL){
-        return {};
-    }
-    inorderTraversal(root->left);
-    v.push_back(root->data);
-    cout<<root->data;
-    inorderTraversal(root->right);
-    return v;
-}
-
+ 
 int main(){
   Node* root= new Node(73);
   root->right=new Node(67);
   root->right->left=new Node(69);
-//   Node* p1=new Node(82);
-//   Node* p2=new Node(84);
-//   root->right=p2;
-//   root->left=p1;
-//   p1->left=new Node(63);
-//   p1->right=new Node(34);
+  Node* p1=new Node(82);
+  Node* p2=new Node(84);
+  root->right=p2;
+  root->left=p1;
+  p1->left=new Node(63);
+  p1->right=new Node(34);
   
-//   p1->left->left=new Node(688);
-//   p1->left->right=new Node(654);
+  p1->left->left=new Node(688);
+  p1->left->right=new Node(654);
 
 
 //   p2->left=new Node(33);
 //   p2->right=new Node(10);
     // cout<<heightofbt(root);
-  vector<int> v = inorderTraversal(root);   
-  for(int i=0;i<v.size();i++){
-    cout<<v[i]<<"  ";
-  } 
+  // vector<int> v = inorderTraversal(root);   
+  // for(int i=0;i<v.size();i++){
+  //   cout<<v[i]<<"  ";
+  // } 
 // cout<<diameterOfBt(root);
 //  pair<int,int> ans = diameterFast(root);
 //  cout<<ans.first;
