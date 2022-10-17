f2465f3b9b9d969a3c925a18386d703db7033a5f
Prasanna Anjankar <prasannaanjankar@gmail.com>
2022-10-17 22:42:12 +0530

    mission greenary

--- a.cpp
+++ a.cpp
@@ -162,31 +162,54 @@ Move iscross(char board[3][3],int i,int j){
     //     }
     return ans;
 }
+// These 2 function togrthrr will return the best possible move for the player
+Move retmove(char board[3][3],int i,int j){
+    Move op1=isrow(board,i,j);
+    Move op2=iscol(board,i,j);
+    Move op3=iscross(board,i,j);
+    if(op1.status==1){
+        return op1;
+    }
+    else if(op2.status==1){
+        return op2;
+    }
+    else if(op3.status==1){
+        return op3;
+    }
+    else{
+        return op1;
+    }
+}
 
-// This will return the best possible move for the player
 Move findBestMove(char board[3][3])
 {
+    Move arr[3],ans[3];
+    int count=0;
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
              if(board[i][j]=='-'){
-             Move op1=isrow(board,i,j);
-             Move op2=iscol(board,i,j);
-             Move op3=iscross(board,i,j);
-            if(op1.status==1){
-                return op1;
-            }
-            else if(op1.status==1){
-                return op1;
-            }
-            else if(op1.status==1){
-                return op1;
-            }
-            else{
-                return op1;
-            }
+                arr[count].row = i;
+                arr[count].col=j;
+                count++;
              }
       }
     }
+
+
+    for(int k=0;k<count;k++){
+         int i=arr[count].row;
+         int j=arr[count].col;
+         ans[k] = retmove(board,i,j);
+    }
+
+    for(int m=0;m<count;m++){
+        if(ans[m].status==1){
+            return ans[m];
+        }
+    }
+    return ans[0];
+
+    // cout<<"count is: "<<count<<"\n";
 }
 
 // Driver code
@@ -196,7 +219,7 @@ int main()
     {
         { 'o', '-', 'x' },
         { 'x', '-', 'x' },
-        { 'o', 'o', 'x' }
+        { 'o', 'o', '-' }
     };
     cout<<"The game till now is given as follows\n";
     Move bestMove = findBestMove(board);
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,24 +1,2 @@
- #include<bits/stdc++.h>
- using namespace std; 
- 
- int thirdLargest(int a[], int n)
-    {
-        int count=1,ans=0;
-         sort(a,a+n);
-         for(int i=0;i<n;i++){
-             
-             if(a[i]!=a[i-1]){
-                 count++;
-             }
-             if(count==3){
-                 ans=a[i];
-                 break;
-             }
-         }
-         return ans;
-    }
-    int main(){
-        int a[]={2,1,3,4,5};
-        int ans=thirdLargest(a,5);
-        cout<<ans;
-    }
+
+    // printf("ROW: %d COL: %d\n\n", bestMove.row,bestMove.col );
