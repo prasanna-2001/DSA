d82b9286d0020f24c2e5baba744c5cf9df34f326
Prasanna Anjankar <prasannaanjankar@gmail.com>
2022-10-24 23:13:07 +0530

    mission greenary

---
+++ apnikaksha+Anuj bhaiyya DSA+striver 180/stl/Map/iterators.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std; 
+int main(){
+    vector<int> v={2,3,5,6,7};
+    //normal iterator
+    // for(int i=0;i<v.size();i++){
+    //     cout<<v[i];
+    // }
+    vector<int>::iterator it;
+    for(it=v.begin();it!=v.end();++it){
+        cout<<(*it)<<" ";
+    }
+cout<<endl;
+    //range based loops:
+    for(int value :v){
+        cout<<value<<" ";
+    }
+}
